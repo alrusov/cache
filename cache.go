ccbe50d551378ec6c33a4ddb7477d98dbd39ed0e
Alexey Rusov <rolic402@mail.ru>
2023-06-18 14:28:18 +0300

    Initial commit

---
+++ cache.go
@@ -0,0 +1,212 @@
+package cache
+
+import (
+	"sync"
+	"time"
+
+	"github.com/alrusov/initializer"
+	"github.com/alrusov/jsonw"
+	"github.com/alrusov/log"
+	"github.com/alrusov/misc"
+)
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+type (
+	Cache struct {
+		mutex *sync.Mutex
+		data  Elems
+	}
+
+	Elems map[string]*Elem
+
+	Elem struct {
+		def
+		cond  *sync.Cond // Для ожидания первого заполнения
+		cache *Cache     // Ссылка на кеш
+		Key   string     `json:"key"` // Ключ
+		Data  any        `json:"-"`   // Данные
+	}
+
+	Stats map[string]Stat
+
+	Stat struct {
+		def
+	}
+
+	def struct {
+		Hash            string    `json:"hash"`            // hash
+		CreatedAt       time.Time `json:"createdAt"`       // Время первоначального создания
+		InProgressFrom  time.Time `json:"inProgressFrom"`  // Время начала обновления
+		LastUpdatedAt   time.Time `json:"lastUpdatedAt"`   // Время последнего обновления
+		ExparedAt       time.Time `json:"exparedAt"`       // Время оуончания жизни
+		Filled          bool      `json:"filled"`          // Зполнено актуальными данными
+		Code            int       `json:"code"`            // http code
+		NumberOfUpdates uint      `json:"numberOfUpdates"` // Количество обновлений
+		NumberOfUses    uint      `json:"numberOfUses"`    // Количество использований
+	}
+)
+
+var (
+	Log     = log.NewFacility("cache")
+	storage *Cache
+)
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+func init() {
+	// Регистрируем инициализатор
+	initializer.RegisterModuleInitializer(initModule)
+}
+
+// Инициализация
+func initModule(appCfg any, h any) (err error) {
+	storage = &Cache{
+		mutex: new(sync.Mutex),
+		data:  make(Elems, 128),
+	}
+
+	return
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+func Get(id uint64, key string, extra ...any) (e *Elem, data any, code int) {
+	return storage.Get(id, key, extra...)
+}
+
+func (c *Cache) Get(id uint64, key string, extra ...any) (e *Elem, data any, code int) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	now := misc.NowUTC()
+
+	hash := makeHash(key, extra)
+	e, exists := c.data[hash]
+	if !exists { // Не существует
+		// Создадим новый
+		e = &Elem{
+			cond:  sync.NewCond(c.mutex),
+			cache: c,
+			Key:   key,
+			def: def{
+				Hash:      hash,
+				CreatedAt: now,
+			},
+		}
+
+		c.data[hash] = e
+		e.debug(id, "new")
+
+	} else { // Уже существует
+		if e.Filled { // Заполнен
+			if now.Before(e.ExparedAt) || // Актуален
+				!e.InProgressFrom.IsZero() { // или в процессе обновления
+				// Берём что дают и уходим
+				code = e.Code
+				data = e.Data
+				e.NumberOfUses++
+
+				e.debug(id, "used")
+				e = nil
+				return
+			}
+
+			// Не актуален и не заполняется, тогда провалимся ниже будем заполнять сами
+			e.debug(id, "updating...")
+
+		} else { // Не заполнен
+			if !e.InProgressFrom.IsZero() { // В процессе заполнения
+				// Будем ждать заполнения
+				e.debug(id, "waiting...")
+				e.cond.Wait()
+				e.debug(id, "resumed")
+
+				// Дождались
+				code = e.Code
+				data = e.Data
+				e.NumberOfUses++
+				e = nil
+				return
+			}
+
+			// Не заполняется, тогда провалимся ниже будем заполнять сами
+		}
+	}
+
+	// Надо заполнять
+	// Вызывающий должен это понять по e != nil, сформировать данные и вызвать e.Commit()
+
+	e.InProgressFrom = now
+
+	return
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+// Данные сформированы, сохраняем
+func (e *Elem) Commit(id uint64, data any, code int, lifetime time.Duration) {
+	e.cache.mutex.Lock()
+	defer e.cache.mutex.Unlock()
+
+	e.InProgressFrom = time.Time{}
+	e.LastUpdatedAt = misc.NowUTC()
+	e.ExparedAt = e.LastUpdatedAt.Add(lifetime)
+	e.Filled = true
+	e.Code = code
+	e.Data = data
+	e.NumberOfUpdates++
+	e.NumberOfUses++
+
+	e.cond.Broadcast()
+
+	e.debug(id, "commited")
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+func makeHash(key string, extra ...any) (hash string) {
+	d := struct {
+		Key   string
+		Extra []any
+	}{
+		Key:   key,
+		Extra: extra,
+	}
+
+	j, _ := jsonw.Marshal(d)
+	hash = string(misc.Sha512Hash(j))
+	return
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+func (e *Elem) debug(id uint64, op string) {
+	if Log.CurrentLogLevel() >= log.DEBUG {
+		j, _ := jsonw.Marshal(e)
+		Log.Message(log.DEBUG, "[%d] %s %s", id, op, j)
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+func GetStat() (s Stats) {
+	return storage.GetStat()
+}
+
+func (c *Cache) GetStat() (s Stats) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	s = make(Stats, len(c.data))
+
+	for key, e := range c.data {
+		s[key] = Stat{
+			def: e.def,
+		}
+	}
+
+	return
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/alrusov/cache
+
+go 1.20
+
+require (
+	github.com/alrusov/initializer v0.1.2
+	github.com/alrusov/jsonw v0.1.3
+	github.com/alrusov/log v0.1.37
+	github.com/alrusov/misc v1.1.8
+)
+
+require (
+	github.com/alrusov/panic v0.1.15 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+)
